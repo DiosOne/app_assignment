6b7527754a57e66dabb4c0e58154546fb7668ca9
DiosOne <domaone83@proton.me>
2025-05-25 17:49:23 +1000

    added enemies stats

---
+++ code/enemies.py
@@ -0,0 +1,38 @@
+class Enemy:
+    def __init__(self, health, armour, lightAttack, heavyAttack):
+        self.health= health
+        self.armour= armour
+        self.attack1= lightAttack
+        self.attack2= heavyAttack
+    
+class Giant_Rat(Enemy):
+    health = int(16)
+    armour= int(13)
+    lightAttack= int(4)
+    heavyAttack= int(8)
+
+class Skeleton(Enemy):
+    health= int(10)
+    armour= int(10)
+    lightAttack= int(6)
+    heavyAttack= int(12)
+
+class Goblin(Enemy):
+    health= int(20)
+    armour= int(15)
+    lightAttack= int(4)
+    heavyAttack= int(8)
+
+def show_stats(cls):
+    print(cls.__name__)
+    print('-' * 10)
+    print(f'Health: {cls.health}')
+    print(f'Armour Class: {cls.armour}')
+    print(f'Light Attack: {cls.lightAttack}')
+    print(f'Heavy Attack: {cls.heavyAttack}')
+    print('*' * 10)
+    print()
+
+show_stats(Giant_Rat)
+show_stats(Skeleton)
+show_stats(Goblin)
