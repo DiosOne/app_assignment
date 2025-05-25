e60079dd40a79f143ab9f5e3d1e0c1454f2b4ba9
DiosOne <domaone83@proton.me>
2025-05-25 18:52:50 +1000

    playing with parameters

--- code/main.py
+++ code/main.py
@@ -1,7 +1,13 @@
 from dice_rolls import dice_roll
 from advent import Adventurer, Fighter, Mage, Ranger, show_stats
-from enemies import Enemy, Goblin, Skeleton, Ratking
+from enemies import Enemy, Goblin, Skeleton, Ratking, show_stats
 
-show_stats(Fighter)
+# show_stats(Fighter)
+print(Fighter.__name__)
+print(f'Health: {Fighter.health}')
 attack_roll= dice_roll('d20')
-print(f'Attack: {attack_roll}')
+damage_roll= dice_roll('d12')
+# print(f'Attack: {attack_roll}')
+if attack_roll <= 10:
+    print(f'You do {damage_roll} points of damage')
+else: print('Your attack missed')
