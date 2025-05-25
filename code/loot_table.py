1843d287dd858682d172bc1346862e870985d393
DiosOne <domaone83@proton.me>
2025-05-25 19:17:37 +1000

    started loot table

---
+++ code/loot_table.py
@@ -0,0 +1,14 @@
+#possible loot
+
+loot_dict= {
+    'Gold': 200,
+    'Precious Gems': 
+        {'Diamonds': 5,
+         'Rubies': 10,
+         'Emeralds': 20,
+         'Garnet': 30
+        },
+    'Rusty Dagger': 20,
+    'Sharp Dagger': 10,
+    'Junk': 40,
+}
