33cc0b73a4d384d27f7c6f9e3d900e8cdee013d8
DiosOne <domaone83@proton.me>
2025-05-25 20:30:16 +1000

    added enemy drop to loot table

--- code/loot_table.py
+++ code/loot_table.py
@@ -1,6 +1,6 @@
 #possible loot
 
-loot_dict= {
+chest_dict= {
     'Gold': 200,
     'Precious Gems': 
         {'Diamonds': 5,
@@ -9,6 +9,17 @@ loot_dict= {
          'Garnet': 30
         },
     'Rusty Dagger': 20,
-    'Sharp Dagger': 10,
+    # 'Sharp Dagger': 10,
     'Junk': 40,
+}
+enemy_drop= {
+    'Gold': 50,
+    'Precious Gems':
+        {'Diamonds': 2,
+         'Rubies': 5,
+         'Emeralds': 10,
+         'Garnet': 10
+        },
+    'Dagger': 10,
+    'Junk': 50
 }
