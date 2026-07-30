19e8044aeadfb0f02f30049782734dfb15078a88
DiosOne <domaone83@proton.me>
2026-07-30 17:43:31 +1000

    chore: fix lib syntax

--- code/rooms.py
+++ code/rooms.py
@@ -32,27 +32,27 @@ rooms= {
     'West Wing':{
         'description':  
             'You are in a medium sized room, with storage shelves and torches on the walls',
-        'exits':{'North': 'Dining Hall', 'East': 'Main Hall', 'West': 'Cupboard'}
+        'exits':{'North':'Dining Hall', 'East':'Main Hall', 'West':'Cupboard'}
     },
 
     'Library':{
         'description': 'A small room with dusty bookshelves lit by numerous cadelabras',
-        'exits':{'West': 'Main Hall'}
+        'exits':{'West':'Main Hall'}
     },
 
     'Dining Hall':{
         'description': 'A large dinning table sits in the center of the room, lit by a chandelier',
-        'exits':{'North': 'Bedroom', 'East': 'Galley', 'South': 'Main Hall', 'West': 'West Wing'}
+        'exits':{'North':'Bedroom', 'East':'Galley', 'South':'Main Hall', 'West':'West Wing'}
     },
 
     'Bedroom':{
         'description': 'A cosy room  with a large bed in the center with torches on the walls',
-        'exits':{'East': 'Dining Hall', 'West': 'Bedroom Cupboard'}
+        'exits':{'East':'Dining Hall', 'West':'Bedroom Cupboard'}
     },
 
     'Scullery':{
         'description': 'A large hearth and stone benches fill the room',
-        'exits':{'North': 'Exit', 'West': 'Dining Hall'}
+        'exits':{'North':'Exit', 'West':'Dining Hall'}
     },
 
     'room-2':{
